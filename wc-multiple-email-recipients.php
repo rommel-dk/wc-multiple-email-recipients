40a6ffbf141a15f18b9c80ea50268f4ce0523751
Con Schneider <cs@conschneider.de>
2017-01-29 19:06:13 +0100

    version bump 1.2

--- wc-multiple-email-recipients.php
+++ wc-multiple-email-recipients.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: WC Multiple Email Recipients 
 Plugin URI: http://conschneider.de/plugin-woocommerce-multiple-email-recipients/
-Description: Allows for multiple recipients for WooCommerce E-Mails
+Description: Allows for multiple recipients for WooCommerce E-Mails. Supports WooCommerce Bookings and WooCommerce Subscriptions.
 Author: Con Schneider
 Author URI: http://conschneider.de/
-Version: 1.1
+Version: 1.2
 */
 
 
